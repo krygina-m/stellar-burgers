1996a4dd3e65ed5c4859679da5ff1b28ef92143d
Maya Krygina <krygina-m@mail.ru>
2026-01-11 14:04:59 +0300

    добавление количества ингридиентов в ingridients-category

---
+++ _src/services/store.ts
@@ -0,0 +1,38 @@
+import { combineReducers, configureStore } from '@reduxjs/toolkit';
+
+import {
+  TypedUseSelectorHook,
+  useDispatch as dispatchHook,
+  useSelector as selectorHook
+} from 'react-redux';
+
+import { ingredientsReducer } from './slices/ingredientsSlice';
+import { userReducer } from './slices/userSlice';
+import { constructorReducer } from './slices/constructorSlice';
+import { orderReducer } from './slices/orderSlice';
+import { feedReducer } from './slices/feedSlice';
+import { userOrdersReducer } from './slices/userOrdersSlice';
+
+const rootReducer = combineReducers({
+  ingredients: ingredientsReducer,
+  user: userReducer,
+  burgerConstructor: constructorReducer,
+  order: orderReducer,
+  feed: feedReducer,
+  userOrders: userOrdersReducer
+});
+
+
+const store = configureStore({
+  reducer: rootReducer,
+  devTools: process.env.NODE_ENV !== 'production'
+});
+
+export type RootState = ReturnType<typeof rootReducer>;
+
+export type AppDispatch = typeof store.dispatch;
+
+export const useDispatch: () => AppDispatch = () => dispatchHook();
+export const useSelector: TypedUseSelectorHook<RootState> = selectorHook;
+
+export default store;
